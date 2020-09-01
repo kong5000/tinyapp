8f935ab594125c89e748117050fc61c5be8fa9f4
Keith Ong <keith.ong5000@gmail.com>
2020-09-01 00:50:08 +0000

    Add body parser and new post endpoint

--- express_server.js
+++ express_server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const STRING_LENGTH = 6;
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  let alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+  let randomIndex;
+  let string = ""
+  for(let i = 0; i < STRING_LENGTH; i++){
+    randomIndex = Math.floor(Math.random() * alphaNumeric.length)
+    string += alphaNumeric[randomIndex];
+  }
+  return string
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,6 +24,11 @@ const urlDatabase = {
 
 app.set("view engine", "ejs")
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +36,12 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   res.render("urls_index", { urls: urlDatabase })
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show",
     {
@@ -23,6 +49,7 @@ app.get("/urls/:shortURL", (req, res) => {
       longURL: urlDatabase[req.params.shortURL],
     })
 });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
